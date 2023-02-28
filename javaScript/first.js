31815d5fca44665f651431d88e89cd68ca376eba
Cybermariam <ademolamariam95@gmail.com>
2023-02-28 11:33:50 +0100

    javascript

---
+++ javaScript/first.js
@@ -0,0 +1,13 @@
+alert("Coming to you from first.js!")
+
+console.log('I"M HERE')
+prompt('yes, if you are ready')
+confirm('press okay if you are ready')
+
+
+let firstName = prompt('What is your first name');
+let lastName = "Lane";
+let fullName = firstName + " " + lastName;
+console.log('hi, ' + firstName + "!") ;
+console.log('how are you doing, ' + firstName + '?');
+console.log('See you later, ' + firstName + '!');
---
+++ javaScript/variable.js
@@ -0,0 +1,7 @@
+let name = "Mariam";
+let dayOfBirth = 25;
+let favoriteColor = prompt("Favorite color");
+console.log("My Favorite Color is " + favoriteColor);
+console.log("I\'m enjoying what i am learning and 'i was giving a question on how to use a single quote and double quotes'")
+console.log("The difference between null and undefined is that undefined is not define i.e, no value was assigned to it while null value is assigned to null")
+console.log("thing is undefined")
